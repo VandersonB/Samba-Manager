9d275825056f6b607acb156b299bd134e27c5839
Henrique Tinelli <rktinelli@gmail.com>
2018-02-24 17:06:13 -0300

    GetAndSetValues
    Leitura de todos os sliders

---
+++ Samba Manager/Assets/Scripts/ResetValues.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetValues : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetFloat("FantasiaCarna", 0);
+        PlayerPrefs.SetFloat("Convidados", 0);
+        PlayerPrefs.SetFloat("Carros", 0);
+
+        PlayerPrefs.SetFloat("Comissao", 0);
+        PlayerPrefs.SetFloat("VelhaGuarda", 0);
+        PlayerPrefs.SetFloat("FantasiaCostura", 0);
+
+        PlayerPrefs.SetFloat("SambaPe", 0);
+        PlayerPrefs.SetFloat("Evolucao", 0);
+        PlayerPrefs.SetFloat("Alas", 0);
+
+        PlayerPrefs.SetFloat("Instrumentos", 0);
+        PlayerPrefs.SetFloat("SambaEnredo", 0);
+        PlayerPrefs.SetFloat("Ritmo", 0);
+
+        PlayerPrefs.SetFloat("Pontuacao", 0);
+    }
+}
---
+++ Samba Manager/Assets/Scripts/ValuesMath.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValuesMath : MonoBehaviour {
+
+    //variaveis do jogador
+    float fantasia_carna;
+    float convidados;
+    float carros;
+
+    float comissao;
+    float velha_guarda;
+    float fantasia_costura;
+
+    float sambape;
+    float evolucao;
+    float alas;
+
+    float instrumentos;
+    float sambaEnredo;
+    float ritmo;
+
+    //variavel de pontuação total
+    float pontosTotal;
+
+    // Use this for initialization
+    void Start () {
+
+        GetAll_values();
+
+        //pontosTotal = formula matematica de pontuacao final
+        //atribuicao a pontuacao final
+        // PlayerPrefs.SetFloat("Pontuacao", pontosTotal);
+
+    }
+
+    private void GetAll_values()
+    {
+        fantasia_carna = PlayerPrefs.GetFloat("FantasiaCarna");
+        convidados = PlayerPrefs.GetFloat("Convidados");
+        carros = PlayerPrefs.GetFloat("Carros");
+
+        comissao = PlayerPrefs.GetFloat("Comissao");
+        velha_guarda = PlayerPrefs.GetFloat("VelhaGuarda");
+        fantasia_costura = PlayerPrefs.GetFloat("FantasiaCostura");
+
+        sambape = PlayerPrefs.GetFloat("SambaPe");
+        evolucao = PlayerPrefs.GetFloat("Evolucao");
+        alas = PlayerPrefs.GetFloat("Alas");
+
+        instrumentos = PlayerPrefs.GetFloat("Instrumentos");
+        sambaEnredo = PlayerPrefs.GetFloat("SambaEnredo");
+        ritmo = PlayerPrefs.GetFloat("Ritmo");
+    }
+
+}
